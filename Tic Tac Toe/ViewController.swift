205b627767ec0bcb6ee4674b15c0e347cef0e7a9
clinares3 <clinares3@cps.edu>
2017-07-11 10:04:01 -0500

    Added the UILabel connections and the UITapGesture is inside

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //These are the variables for the spots where the emoji values are at
+    @IBOutlet weak var box1: UILabel!
+    
+    @IBOutlet weak var box2: UILabel!
+    
+    @IBOutlet weak var box3: UILabel!
+    
+    @IBOutlet weak var box4: UILabel!
+    
+    @IBOutlet weak var box5: UILabel!
+    
+    @IBOutlet weak var box6: UILabel!
+    
+    @IBOutlet weak var box7: UILabel!
+    
+    @IBOutlet weak var box8: UILabel!
+    
+    @IBOutlet weak var box9: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   //Already there. Don't touch.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
